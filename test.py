227e2f2a8d03ac38dfc3bc67118db767b62abe41
Devarshi Hazarika <49090172+deva24@users.noreply.github.com>
2020-05-13 15:20:05 +0530

    all lines are found

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import math
 
-poly_interest = 79
+poly_interest = 99
 
 
 def capture():
@@ -20,6 +20,10 @@ def capture():
 
 def distance(a, b):
     return math.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
+
+
+def angle1(a, b):
+    return math.atan2(b[1]-b[0], a[1]-a[0])*180/math.pi
 
 
 def deviation(a, b, c):
@@ -46,6 +50,122 @@ ind = 0
 mind = -1
 printit = False
 
+def findLines(cont):
+    i = 0
+    len_c = len(cont)
+
+    predictedLines = []
+    predictedLine = None
+
+    acc_line = []
+    lineWasFound = False
+
+    pa = None
+    pb = None
+    pc = None
+
+    dis_ab = None
+    dis_bc = None
+    dis_ac = None
+
+    ang_ab = None
+    ang_bc = None
+    ang_ac = None
+
+    while i < len_c:
+        if type(pa) == type(None):
+            pa = cont[i % len_c][0]
+        if type(pb) == type(None):
+            pb = cont[(i+1) % len_c][0]
+        if type(pc) == type(None):
+            pc = cont[(i+2) % len_c][0]
+
+        if type(dis_ab) == type(None):
+            dis_ab = distance(pa, pb)
+        if type(dis_bc) == type(None):
+            dis_bc = distance(pb, pc)
+        if type(dis_ac) == type(None):
+            dis_ac = distance(pa, pc)
+
+        # if type(ang_ab) == type(None):
+        #     ang_ab = angle1(pa, pb)
+        # if type(ang_bc) == type(None):
+        #     ang_bc = angle1(pb, pc)
+        # if type(ang_ac) == type(None):
+        #     ang_ac = angle1(pa, pc)
+
+        didMerge = False
+
+        if dis_ab + dis_bc - dis_ac <= 0.5:
+            didMerge = True
+
+        if didMerge:
+            if not lineWasFound:
+                predictedLine = [(i) % len_c, (i+1) % len_c]
+                predictedLines.append(predictedLine)
+            else:
+                predictedLine[1] = (i+1) % len_c
+
+            if len(acc_line) == 0:
+                acc_line.append(i)
+            acc_line.append(i+1)
+
+            # change variable like
+            #
+            # A--B--C--d
+            #     |
+            #     V
+            # A-----B==C
+
+            # pa carry over
+            pb = pc
+            pc = None
+
+            dis_ab = dis_ac
+            dis_bc = None
+            dis_ac = None
+
+            # ang_ab = ang_ac
+            # ang_bc = None
+            # ang_ac = None
+
+            lineWasFound = True
+
+        else:
+            # remove predicted line if its too short
+            if type(predictedLine) != type(None):
+                pl1 = cont[predictedLine[0]%len_c][0]
+                pl2 = cont[(predictedLine[1]+1)%len_c][0]
+
+                if distance(pl1,pl2) < 5:
+                    predictedLines.pop()
+                predictedLine=None
+
+            # change variable like
+            #
+            # A--B--C--D
+            #     |
+            #     V
+            # z--A--B==C
+
+            pa = pb
+            pb = pc
+            pc = None
+
+            dis_ab = dis_bc
+            dis_bc = None
+            dis_ac = None
+
+            # ang_ab = ang_bc
+            # ang_bc = None
+            # ang_ac = None
+
+            lineWasFound = False
+            # end if
+        i += 1
+        # end while i < len_cc
+    return predictedLines
+
 
 def qrcodescan(frame):
     global ind, printit, mind, debugit
@@ -57,108 +177,34 @@ def qrcodescan(frame):
     contours, _ = cv2.findContours(
         thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    fromAngle = -30
-    toAngle = 30
-
     #contours = [contours[poly_interest]]
 
     if not debugit:
-
-        # algorithm tries to find a 4 straight lines
-        #
-        # A--B
-        # |  |
-        # D--C
-        # 
-
-
         for cont in contours:
+            predictedLines = findLines(cont)
+
             len_c = len(cont)
+            colors = [ (0, 175, 255), (0, 180, 0),(255,255,0), (255, 0, 0)]
+            firstColor =(0, 0, 255)
+            ic = 0
+            lc = len(colors)
+            doFirstColor=True
+            for lineIndices in predictedLines:
+                pa = cont[lineIndices[0] % len_c][0]
+                pb = cont[(lineIndices[1]+1) % len_c][0]
+                if doFirstColor:
+                    doFirstColor=False
 
-            foundAB = False
-            fromAngle = -30
-            toAngle = 30
+                    cv2.line(frame, toplePoint(pa),
+                         toplePoint(pb), firstColor, 2)
+                else:
+                    cv2.line(frame, toplePoint(pa),
+                         toplePoint(pb), colors[ic % lc], 2)
+                ic += 1
 
-            attempt = 0
-
-            # attempt to find line AB
-            while attempt < 2:
-                lineAB = []
-                i = int(len_c * 0.4)
-                loop_over_len = int(len_c*1.6)
-
-                while i < loop_over_len:
-                    pa = cont[(i-1) % len_c][0]
-                    pb = cont[i % len_c][0]
-                    pc = cont[(i+1) % len_c][0]
-                    pd = cont[(i+2) % len_c][0]
-
-                    angle_ab = math.atan2(pb[1]-pa[1], pb[0]-pa[0])/math.pi*180
-                    angle_bc = math.atan2(pc[1]-pb[1], pc[0]-pb[0])/math.pi*180
-                    angle_cd = math.atan2(pd[1]-pc[1], pd[0]-pc[0])/math.pi*180
-
-                    dis_ab = distance(pa, pb)
-                    dis_bc = distance(pb, pc)
-                    dis_cd = distance(pc, pd)
-
-                    lookslikeLine = False
-
-                    if dis_bc > dis_ab and dis_bc > dis_cd:
-                        if angle_bc >= fromAngle and angle_bc <= toAngle:
-                            lookslikeLine = True
-                    elif len(lineAB)>0 and angle_bc >= fromAngle and angle_bc <= toAngle:
-                        lookslikeLine=True
-                    elif dis_ab > dis_bc and dis_cd > dis_bc:
-                        if angle_ab >= fromAngle and angle_ab <= toAngle and angle_cd >= fromAngle and angle_cd <= toAngle:
-                            lookslikeLine = True
-                    
-
-                    if lookslikeLine:
-                        lineAB.append(i)
-
-                    else:
-                        if len(lineAB) > 0:
-                            lineIndexBegin = lineAB[0]
-                            lineIndexEnd = lineAB[len(lineAB)-1] + 1
-
-                            pa = cont[lineIndexBegin % len_c][0]
-                            pb = cont[lineIndexEnd % len_c][0]
-                            if distance(pa, pb) > 5:
-                                foundAB = True
-                                break
-                            else:
-                                lineAB = []
-                    i += 1
-                if foundAB:
-                    # line stores index i of a line consisting of point[i] and point[i+1] in contour
-                    lineIndexBegin = lineAB[0]
-                    lineIndexEnd = lineAB[len(lineAB)-1] + 1
-
-                    pa = cont[lineIndexBegin % len_c][0]
-                    pb = cont[lineIndexEnd % len_c][0]
-
-                    if distance(pa, pb) >= 5:
-                        if attempt == 0:
-                            cv2.line(frame, toplePoint(pa),
-                                toplePoint(pb), (0, 0, 255), 2)
-                        else:
-                            cv2.line(frame, toplePoint(pa),
-                                toplePoint(pb), (0, 100, 255), 2)
-                        break
-                    else:
-                        foundAB=False
-
-
-                fromAngle += 45
-                toAngle +=45
-                attempt += 1
-    
-            # if no AB like line found then skip lower code
-            if not foundAB:
-                continue
-
-            # attempt to find line BC
-
+            # end for cont in contours
+        cv2.line(frame,(10,10),(15,10),(0,0,255),2)
+        # end if not degugit
 
     else:
         cont = contours[ind]
@@ -171,7 +217,7 @@ def qrcodescan(frame):
                 cv2.line(frame, toplePoint(pa), toplePoint(pb), (0, 0, 255), 2)
                 i += 1
             if printit:
-                print('index1 ' + str(ind))
+                print('index ' + str(ind))
                 print('=======')
         else:
             mind = mind % len_c
@@ -269,7 +315,7 @@ def scan():
     cv2.destroyAllWindows()
 
 
-# checkfn()
-scan()
+checkfn()
+#scan()
 
 exit
